673a5b03805d85655b0102e55ced4fbfd7cae020
adgutierrez <adgutierrez@techbyte.net>
2022-06-20 20:05:51 -0700

    First script, comments, Start() and Update(). Move forward with Vector3, Time.deltaTime. Add RigidBody component. Cltr D to duplicate objects. Lesson 1.2 completed

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We will move the vehicle forward
+        transform.Translate(moveSpeed * Time.deltaTime * Vector3.forward);
+    }
+}
